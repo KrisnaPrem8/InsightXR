ccd0e341168f3d807b87bba8f75f00a565d7f7e4
KrisnaPrem <krisna@detecttechnologies.com>
2024-05-17 11:08:42 -0700

    First commit

---
+++ Assets/Scripts/HeatMapManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HeatMapManager : MonoBehaviour
+{
+    // Declaration of the heatmap particle effect prefab
+    public GameObject HeatMapParticleEffectPrefab;
+    void FixedUpdate()
+    {
+        // Initialization of ray with current position as position and forward vector as direction
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Instantiating the particle effect prefab at the hit point of other collider
+            Instantiate(HeatMapParticleEffectPrefab, hit.point, Quaternion.identity);
+        }
+    }
+}
+
+
+
+
+
