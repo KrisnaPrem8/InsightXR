162bd7c4f3575f52466cb9c71df25404b5158d65
KrisnaPrem <krisna@detecttechnologies.com>
2024-05-17 12:15:27 -0700

    Script Changes

--- Assets/Scripts/HeatMapManager.cs
+++ Assets/Scripts/HeatMapManager.cs
@@ -13,7 +13,8 @@ public class HeatMapManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             // Instantiating the particle effect prefab at the hit point of other collider
-            Instantiate(HeatMapParticleEffectPrefab, hit.point, Quaternion.identity);
+            GameObject heat = Instantiate(HeatMapParticleEffectPrefab, hit.point, Quaternion.identity);
+            Destroy(heat, 1f);
         }
     }
 }
